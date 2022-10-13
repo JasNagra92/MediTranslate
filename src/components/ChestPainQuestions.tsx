2974eac08e544e8351023a9d5b3b0278b20ccefe
jasmac <jaskarannagra@hotmail.com>
2022-10-12 20:01:10 -0700

    extract each question into question component that will render correct information with props

--- src/components/ChestPainQuestions.tsx
+++ src/components/ChestPainQuestions.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {TouchableOpacity, StyleSheet, View, Text} from 'react-native';
 import SoundPlayer from 'react-native-sound-player';
+import Question from './Question';
 
 const ChestPainQuestions: React.FC<{}> = () => {
-  const handlePress = () => {
+  const handlePress = (filename: string) => {
     try {
-      SoundPlayer.playSoundFile('test', 'mp3');
+      SoundPlayer.playSoundFile(filename, 'mp3');
     } catch (error) {
       console.log(error);
     }
@@ -13,19 +14,16 @@ const ChestPainQuestions: React.FC<{}> = () => {
 
   return (
     <View style={style.container}>
-      <View style={style.container}>
-        <TouchableOpacity
-          style={style.button}
-          activeOpacity={0.7}
-          onPress={() => handlePress()}>
-          <Text style={style.text}>Are you having Chest pain?</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={style.container}>
-        <TouchableOpacity style={style.button} activeOpacity={0.7}>
-          <Text style={style.text}>Are you Nauseated?</Text>
-        </TouchableOpacity>
-      </View>
+      <Question
+        question="Are you having Chest Pain?"
+        onPress={handlePress}
+        filename="test"
+      />
+      <Question
+        question="Are you Nauseated?"
+        onPress={handlePress}
+        filename="q2cp"
+      />
     </View>
   );
 };
@@ -35,20 +33,6 @@ const style = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  button: {
-    padding: 40,
-    width: '100%',
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: 'green',
-    backgroundColor: 'lightblue',
-  },
-  text: {
-    textAlign: 'center',
-    fontSize: 20,
-    color: 'black',
-    fontWeight: '900',
   },
 });
 
---
+++ src/components/Question.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {TouchableOpacity, StyleSheet, View, Text} from 'react-native';
+interface Props {
+  question: string;
+  filename: string;
+  onPress: (filename: string) => void;
+}
+
+const Question: React.FC<Props> = ({question, filename, onPress}) => {
+  return (
+    <View style={style.container}>
+      <TouchableOpacity
+        style={style.button}
+        activeOpacity={0.7}
+        onPress={() => onPress(filename)}>
+        <Text style={style.text}>{question}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+export default Question;
+const style = StyleSheet.create({
+  container: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    padding: 40,
+    width: '100%',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'green',
+    backgroundColor: 'lightblue',
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: 'black',
+    fontWeight: '900',
+  },
+});
